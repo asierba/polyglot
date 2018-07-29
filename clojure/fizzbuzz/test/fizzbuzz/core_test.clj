972d8625342dd5bbfef804a8f2b3ca665941c0df
Asier <1537266+asierba@users.noreply.github.com>
2018-07-29 17:04:53 +0100

    Clojure fizzbuzz: main program prints fizzbuzz from 1 to 100

--- clojure/fizzbuzz/test/fizzbuzz/core_test.clj
+++ clojure/fizzbuzz/test/fizzbuzz/core_test.clj
@@ -1,16 +1,6 @@
-(ns kataskeleton.core-test
+(ns fizzbuzz.core-test
     (:require [clojure.test :refer :all]
-      [kataskeleton.core :refer :all]))
-
-(defn multiple-of? [n, m]
-      (= (mod n m) 0))
-
-(defn fizzbuzz [number]
-      (cond
-        (multiple-of? number 15) "fizzbuzz"
-        (multiple-of? number 5) "buzz"
-        (multiple-of? number 3) "fizz"
-        :else (str number)))
+      [fizzbuzz.core :refer :all]))
 
 (deftest a-test
          (testing "fizzbuzz of 1 is 1"
@@ -39,3 +29,7 @@
 (deftest a-test
          (testing "fizzbuzz of 15 is fizzbuzz"
                   (is (= (fizzbuzz 15) "fizzbuzz"))))
+
+(deftest a-test
+         (testing "fizzbuzz from 1 to 10"
+                  (is (= (fizzbuzz-to 10) '("1" "2" "fizz" "4" "buzz" "fizz" "7" "8" "fizz" "buzz")))))
