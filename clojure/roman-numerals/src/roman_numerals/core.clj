b1cd321c24d0f1b152573ffa6cf455eb805f8a06
Asier <1537266+asierba@users.noreply.github.com>
2018-07-31 07:54:39 +0100

    Clojure: continue with roman numerals

--- clojure/roman-numerals/src/roman_numerals/core.clj
+++ clojure/roman-numerals/src/roman_numerals/core.clj
@@ -1,12 +1,22 @@
 (ns roman-numerals.core
   (:gen-class))
 
+(def arabic-to-roman
+  {10 "X",
+   5 "V",
+   1 "I",
+   }
+  )
+
 (defn roman-of [arabic]
   (cond
-    (= arabic 3) "III"
-    (= arabic 2) "II"
-    (= arabic 1) "I"
-    :default ""))
+    (<= arabic 0) ""
+    (contains? arabic-to-roman arabic) (arabic-to-roman arabic)
+    :default (cond
+               (= arabic 20) (str (roman-of 10) (roman-of 10))
+               (= arabic 15) (str (roman-of (- arabic 5)) (roman-of 5))
+               :default (str (roman-of (- arabic 1)) (roman-of 1)))
+              ))
 
 (def decimal 1)
 (def roman (roman-of decimal))
--- clojure/roman-numerals/test/roman_numerals/core_test.clj
+++ clojure/roman-numerals/test/roman_numerals/core_test.clj
@@ -2,10 +2,18 @@
   (:require [clojure.test :refer :all]
             [roman-numerals.core :refer :all]))
 
+
 (deftest roman-of-arabic
   (testing "Roman of arabic"
            (is (= (roman-of 0) ""))
+           (is (= (roman-of -1) ""))
            (is (= (roman-of 1) "I"))
            (is (= (roman-of 2) "II"))
            (is (= (roman-of 3) "III"))
+           (is (= (roman-of 5) "V"))
+           (is (= (roman-of 6) "VI"))
+           (is (= (roman-of 10) "X"))
+           (is (= (roman-of 15) "XV"))
+           (is (= (roman-of 16) "XVI"))
+           (is (= (roman-of 20) "XX"))
            ))
