8f2454afd2a84f4dc3c9befad8aadce7380676c7
Asier <1537266+asierba@users.noreply.github.com>
2019-10-28 23:10:09 +0100

    refactor scala fizzbuz to use pattern matching

--- scala/fizzbuzz/src/main/scala/FizzBuzz.scala
+++ scala/fizzbuzz/src/main/scala/FizzBuzz.scala
@@ -1,17 +1,15 @@
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-    println("Fizz buzz from 1 to 100")
-    (1 to 100).foreach(num => println(s"$num. - ${of(num)}"))
+    val range = 1 to 100
+    println(s"Fizz buzz for $range")
+    range.foreach(num => println(s"$num. - ${of(num)}"))
   }
 
-  def of(value: Int): String = {
-    if (value % 15 == 0)
-      "fizzbuzz"
-    else if (value % 5 == 0)
-      "buzz"
-    else if (value % 3 == 0)
-      "fizz"
-    else
-      value.toString
-  }
+  def of(value: Int): String =
+    (value % 5, value % 3) match {
+      case (0, 0) => "fizzbuzz"
+      case (_, 0) => "fizz"
+      case (0, _) => "buzz"
+      case _ => value.toString
+    }
 }
--- scala/fizzbuzz/src/test/scala/FizzBuzzSpec.scala
+++ scala/fizzbuzz/src/test/scala/FizzBuzzSpec.scala
@@ -2,21 +2,21 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
   it should "be number for ohters" in {
-    FizzBuzz.of(1) should be ("1")
-    FizzBuzz.of(2) should be ("2")
-    FizzBuzz.of(4) should be ("4")
+    FizzBuzz of 1 should be ("1")
+    FizzBuzz of 2 should be ("2")
+    FizzBuzz of 4 should be ("4")
   }
   it should "be fizz for multiple of 3" in {
-    FizzBuzz.of(3) should be ("fizz")
-    FizzBuzz.of(6) should be ("fizz")
+    FizzBuzz of 3 should be ("fizz")
+    FizzBuzz of 6 should be ("fizz")
   }
   it should "be buzz for multiple of 5" in {
-    FizzBuzz.of(5) should be ("buzz")
-    FizzBuzz.of(10) should be ("buzz")
-    FizzBuzz.of(20) should be ("buzz")
+    FizzBuzz of 5 should be ("buzz")
+    FizzBuzz of 10 should be ("buzz")
+    FizzBuzz of 20 should be ("buzz")
   }
   it should "be fizzbuzz for multiple of 15" in {
-    FizzBuzz.of(15) should be ("fizzbuzz")
-    FizzBuzz.of(30) should be ("fizzbuzz")
+    FizzBuzz of 15 should be ("fizzbuzz")
+    FizzBuzz of 30 should be ("fizzbuzz")
   }
 }
